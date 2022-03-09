78b9e0311f45d53b7c64ea4a23a23cac1bbdd741
pcancio <pcancio@gmail.com>
2022-03-08 20:11:51 -0500

    typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,31 +1,58 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Thought {
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  cards: [Cards]
+  contacts: [User]
+
+}
+  type Card {
     _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
+tagline: String
+preferredName: String
+pronouns: String
+title: String
+company: String
+email: String
+phone: String
+linkedIn: String
+Instagram: String
+website: String
   }
 
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    thoughts: [Thought]!
-    thought(thoughtId: ID!): Thought
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+}
+  type Query {
+    cards: [Cards]!
+    card(_id: ID!): Cards
   }
 
-  type Mutation {
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
-  }
+  // type Mutation {
+  //   login(email: String!, password: String!): Auth
+  //   addUser(username: String!, email: String!, password: String!): Auth
+  //   addCard(cardText: String!): Thought
+  //   addContact(_id: ID!): User
+  // }
+
+  //   addUser(thoughtText: String!, thoughtAuthor: String!): Thought
+  //   addComment(thoughtId: ID!, commentText: String!): Thought
+  //   removeThought(thoughtId: ID!): Thought
+  //   removeComment(thoughtId: ID!, commentId: ID!): Thought
+  // }
 `;
 
 module.exports = typeDefs;
