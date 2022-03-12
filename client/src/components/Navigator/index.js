7e30f2bdfc751f72d84dbced880872ab5ab8fa3d
pcancio <pcancio@gmail.com>
2022-03-12 16:45:29 -0500

    Navigator, Login, Signup components

---
+++ client/src/components/Navigator/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import {} from "react-bootstrap";
+
+function Navigator() {
+  return (
+    <header>
+      <Navbar bg="light">
+        <Container>
+          <NavbarBrand href="/">QRad</NavbarBrand>
+          <Nav className="me-auto">
+            <Nav.Link href="#my-profile">My Profile</Nav.Link>
+            <Nav.Link href="#my-contacts">My Contacts</Nav.Link>
+            <Nav.Link href="#my-qr">My QR Code</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+export default Navigator;
