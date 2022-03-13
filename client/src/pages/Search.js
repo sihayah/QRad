637e50b40d25b917167ff260ac1cbda11d8f078a
Marcio R <Murcielago203@gmail.com>
2022-03-13 15:48:50 -0400

    updated map function in search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,18 +1,18 @@
 import "";
-import JSONDATA from "../data/MOCK_DATA.json";
-import {useState} from 'react';
+import json from "../data/MOCK_DATA.json";
+import {useState} from 'react'
 
 function Search() {
     const [searchTerm, setSearchTerm] = useState("")
-    return (<div className="App">
+    return (<div className="search">
         <input type="text" 
         placeholder="Search Contacts..." 
         onChange={event => {setSearchTerm(event.target.value);
          }}
         />
-        {JSONDATA.filter((val) => {
-            if (searchTerm == "") {
-               return val 
+        {json.filter((val) => {
+            if (searchTerm ==="") {
+               return ('')
             } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
                 return val
             }
@@ -20,6 +20,11 @@ function Search() {
             return (
             <di className="user" key={key}>
                      <p>{val.first_name}</p>
+                     <p>{val.last_name}</p>
+                     <p>{val.company_name}</p>
+                     <p>{val.email}</p>
+                     <p>{val.phone}</p>
+                     
             </di>
             );
         })}
