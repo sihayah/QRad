fd0b39a423a8d0654c8e0cbed99fc8fadbb9dd24
Sihayah <sihaya_a@hotmail.com>
2022-03-14 19:27:10 -0400

    updated typedefs to include firstname and lastName

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,7 +12,8 @@ const typeDefs = gql`
   type Card {
     _id: ID
     tagline: String
-    preferredName: String
+    firstName: String
+    lastName: String
     pronouns: String
     title: String
     company: String
@@ -39,8 +40,8 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth!
     addUser(username: String!, email: String!, password: String!): Auth!
-    addCard(tagline: String, preferredName: String, pronouns: String, title: String, company: String, email: String!, phone: String, linkedIn: String, instagram: String, website: String): Card
-    updateCard(_id: ID!, tagline: String, preferredName: String, pronouns: String, title: String, company: String, email: String, phone: String, linkedIn: String, instagram: String, website: String): Card
+    addCard(tagline: String, firstName: String, lastName: String, pronouns: String, title: String, company: String, email: String!, phone: String, linkedIn: String, instagram: String, website: String): Card!
+    updateCard(_id: ID!, tagline: String, firstName: String, lastName: String, pronouns: String, title: String, company: String, email: String, phone: String, linkedIn: String, instagram: String, website: String): Card!
     addContact(_id: ID!): User!
     removeContact(_id: ID!): User!
   }
