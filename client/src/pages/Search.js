465be200fb88f3832ffa4504b6cd69ae78cac7ee
Marcio R <Murcielago203@gmail.com>
2022-03-14 16:42:12 -0400

    updated search bart function to query thourgh all json fields , and render , created button to show contact info

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,35 +1,58 @@
-import "";
-import json from "../data/MOCK_DATA.json";
-import {useState} from 'react'
+import "./App.css";
+import json from "./MOCK_DATA.json";
+import React, {useState} from 'react'
 
 function Search() {
-    const [searchTerm, setSearchTerm] = useState("")
-    return (<div className="search">
+  
+    const [searchTerm, setSearchTerm] = useState('')
+    const [show, setShow] = useState(false)
+    
+   
+    return (
+      <div className="App">
+        <center>
         <input type="text" 
         placeholder="Search Contacts..." 
         onChange={event => {setSearchTerm(event.target.value);
          }}
         />
+        <hr/>
+        
         {json.filter((val) => {
             if (searchTerm ==="") {
-               return ('')
+               return ('') 
             } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            } else if (val.last_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            } else if (val.phone.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            }else if (val.website.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            }else if (val.linkedin.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            }else if (val.twitter.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val
+            }else if (val.instagram.toLowerCase().includes(searchTerm.toLowerCase())) {
                 return val
             }
         }).map((val, key)=> {
+
             return (
-            <di className="user" key={key}>
-                     <p>{val.first_name}</p>
-                     <p>{val.last_name}</p>
-                     <p>{val.company_name}</p>
-                     <p>{val.email}</p>
-                     <p>{val.phone}</p>
-                     
-            </di>
+            <div className="user" key={key}> 
+            <p>{val.first_name} , {val.last_name}</p>
+                {              
+                show?<p>{val.email} <p>{val.phone}</p> <p>{val.company_name}</p> <p>{val.website}</p> <p>{val.linkedin}</p> <p>{val.twitter}</p> <p>{val.instagram}</p></p>:null
+                }
+
+             <button onClick={() =>setShow(!show)}>Show Contact Info</button>
+             
+            </div>
             );
         })}
+        </center>
     </div>
     );
 }
 
-export default Search;
+export default Search ;
