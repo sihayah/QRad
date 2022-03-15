46fee8acac9b47e7afd43e2db0cc99ee543147ca
Sihayah <sihaya_a@hotmail.com>
2022-03-15 15:16:38 -0400

    added icons and conditional rendering to all data not required in Card model

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,30 +1,73 @@
 import React from 'react';
 import './style.css';
+import {HiOutlineMailOpen } from 'react-icons/hi';
+import {BsFillTelephoneForwardFill} from 'react-icons/bs';
+import {IoIosBusiness} from 'react-icons/io';
+import {CgWebsite} from 'react-icons/cg';
+import {AiFillInstagram} from 'react-icons/ai';
 
-const Card = ({ cards }) => {
+const Card = ({ data }) => {
 
-    if (!cards) {
-        return <h3>No cards here yet!</h3>;
+    if (!data) {
+        return <h3>No data here yet!</h3>;
       }
     
     return(
         <>
         <div className="card">
             <div className='intro'>
-                <h4>prefferedName</h4>
-                <p className="pronouns">({cards.pronouns})</p>
-                <p className="tagline">{cards.tagline}</p>     
+                <h4>{data.firstName} {data.lastName}</h4>
+                {data.pronouns && (
+                    <p className="pronouns">({data.pronouns})</p>
+                )}
+                {data.tagline && (
+                    <p className="tagline">{data.tagline}</p>
+                )} 
             </div>
-            <div className='card-img'>
-                <img src = {cards.img}/>
-            </div>
+            {data.img && (
+                <div className='card-img'>
+                    <img alt ={data.usernmae} src={data.img}/>
+                </div>
+                )} 
             <ul>
-                <li>{cards.company}{cards.title}</li>
-                <li>{cards.email}</li>
-                <li>{cards.phone}</li>
-                <li><a href={cards.website}>{cards.website}</a></li>
-                <li>{cards.linkedIn}</li>
-                <li>{cards.instagram}</li>
+                {data.company && (
+                    <li>
+                        <IoIosBusiness /> 
+                        {data.company}
+                    </li>
+                )}
+                {data.title && (
+                    <li>
+                        {data.title}
+                    </li>
+                )}
+                <li>
+                    <HiOutlineMailOpen /> {data.email}
+                </li>
+                {data.phone && (
+                    <li>
+                        <BsFillTelephoneForwardFill /> 
+                        {data.phone}
+                    </li>
+                )}
+                {data.website && (
+                    <li>
+                        <CgWebsite />
+                        <a href={data.website}> {data.website}</a>
+                    </li>
+                )}
+                {data.linkedIn && (
+                    <li>
+                        <CgWebsite /> 
+                        {data.linkedIn}
+                    </li>
+                )}
+                {data.instagram && (
+                    <li>
+                        <AiFillInstagram />  
+                        {data.instagram}
+                    </li>
+                )}
             </ul>            
         </div>
         </>
