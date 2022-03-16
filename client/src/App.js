3999748e8b6f03e34e99d7cad896bf912c8e2246
Christian Diaz <christian55diaz@gmail.com>
2022-03-15 21:20:02 -0400

    removed nonsense code used as a template

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,22 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Search from "./pages/Search";
-import Profile from "./pages/Profile/EditForm";
+
+import Dashboard from './pages/UserDash'
+import Home from './pages/Home';
+import Header from './components/Header';
+
+import Search from './pages/Search';
 // import Qrcode from './components/Qrcode;
+// import Profile from './components/Profile';
 // import Contacts from './components/Contacts';
 // import Card from './components/Card;
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import { setContext } from "@apollo/client/link/context";
+// // import Login from "./components/Login";
+// import Signup from "./components/Signup";
 
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLInk = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
 
 const client = new ApolloClient({
-  link: authLInk.concat(httpLink),
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
@@ -40,38 +24,26 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        {/* <div className="flex-column justify-flex-start min-100-vh"> */}
-        <Header />
-        <div className="container">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-          {/* Create a route to display a single thought's comments based on its `thoughtId` provided in the URL */}
-          <Route exact path="/thoughts/:thoughtId">
-            {/* <SingleThought /> */}
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
 
-          {/* <Route exact path="//:thoughtId">
+        {/* <div className="flex-column justify-flex-start min-100-vh"> */}
+          <Header />
+          <div className="container">
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route exact path="/sign-up">
+              {/* <Signup /> */}
+            </Route>
+
+            {/* <Route exact path="//:thoughtId">
               <Contacts /> */}
-          {/* </Route> */}
-        </div>
-        {/* <Footer /> */}
+            {/* </Route> */}
+          </div>
+          {/* <Footer /> */}
         {/* </div> */}
+
       </Router>
     </ApolloProvider>
   );
