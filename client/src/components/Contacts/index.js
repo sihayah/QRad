73709171b3e72e1e4af10025129e66b37165b1f9
sihayah <91216737+sihayah@users.noreply.github.com>
2022-03-16 05:26:52 -0400

    removed old commented logic

--- client/src/components/Contacts/index.js
+++ client/src/components/Contacts/index.js
@@ -1,29 +1,15 @@
-// import React from 'react';
-// import Search from '../../pages/Search';
-// import './contacts.css';
-
-// function Contacts() {
-//     return (
-//         <div className='contact-page'>
-//             <h1>my contacts</h1>
-//             <Search></Search>
-//         </div>
-//     )
-// }
-
-// export default Contacts;
-
 import React from 'react'
 import Card from '../Card';
 
 const ContactList = ({ username, contacts }) => {
-
-    
     return(
         <>
             <h3>{username}'s Contacts</h3>
             {contacts.map(contact => (
-                <Card data={contact}/>
+                <div key={contact._id}>
+                    <Card data={contact.cards[0]}/>    
+                </div>
+                
             ))}        
         </>
     )
