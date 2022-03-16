6b5421b1d8a9bb63515ab5e8351841de7bb609b8
sihayah <91216737+sihayah@users.noreply.github.com>
2022-03-16 03:43:39 -0400

    updated MyCard compt to use QUERY_USER instead of QUERY_ME

--- client/src/pages/MyCard/index.js
+++ client/src/pages/MyCard/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Card from '../../components/Card';
-import { QUERY_ME } from '../../utils/queries';
+import { QUERY_USER } from '../../utils/queries';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 const Profile = () => {
 
-const { data } = useQuery(QUERY_ME);
-console.log(data.me._id)
+  const { id } = useParams();  
+  const { loading, data } = useQuery(QUERY_USER, {
+      variables: { _id: id }
+    });
 
-  return(
-    <>
-        <Card data={data.me.cards[0]} />
-    </>
-  )    
+  if (loading) {
+    return(
+      <h4>Loading...</h4>
+    )
+  }
+  if (data) {
+    return(
+      <>
+          <Card data={data.user.cards[0]} />
+      </>
+    )      
+  }
+
 }
 
 export default Profile;
