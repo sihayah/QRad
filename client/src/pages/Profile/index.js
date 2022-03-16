02780aae0c5157d6b20fd25164cc2fc025c2e007
Christian Diaz <christian55diaz@gmail.com>
2022-03-15 22:54:03 -0400

    userProfile trying to execute me Queries settingup profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,70 +1,71 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
-import { ADD_CONTACT } from '../utils/mutations';
-import { useMutation } from '@apollo/client';
 
-const Profile = () => {
-  const [addContact] = useMutation(ADD_CONTACT);
+import { Link } from 'react-router-dom';
+
+import { QUERY_USER, QUERY_ME } from '../../utils/queries';
+import { useQuery } from '@apollo/client';
+import Auth from '../../utils/auth';
+import me from '../../MOCK_ME.json';
+
+const Profile = (props) => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
 
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/profile" />;
-  }
+ // redirect to personal profile page if username is yours
+ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  return <Redirect to="/profile" />;
+}
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+if (loading) {
+  return <div>Loading...</div>;
+}
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Log in or sign up!
-      </h4>
-    )
-  }
-
-  const handleClick = async () => {
-    try {
-      await addContact({
-        variables: { id: user._id }
-      });
-    } catch (e) {
-      console.error(e);
-    }
+if (!user?.username) {
+  return (
+    <h4>
+      You need to be logged in to see this. Use the navigation links above to
+      sign up or log in!
+    </h4>
+  );
+}
+  if (!me.cards) {
+      return(
+          <>
+            <Link to='/editform'>
+              Create my QRad 
+            </Link>
+              
+          </>
+      )
   };
 
-  return (
-    <div>
-      <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : 'your'} card.
-      </h2>
+  return(
+    <Link to='/editform'>
+      Update my QRad
 
-      {userParam && (
-        <button className='btn ml-auto' onClick={handleClick}>
-          Add Contact
-        </button>
-      )}
-      </div>
+  <div>
+  <div className="flex-row mb-3">
+    <h2 className="bg-dark text-secondary p-3 display-inline-block">
+      Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+    </h2>
+  </div>
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          {/* this will be the card */}
-        </div>
-      </div>
-      
-      <div className="mb-3">{!userParam}</div>
+    <div className="col-12 col-lg-3 mb-3">
+      <Contacts
+        username={user.username}
+        friends={user.friends}
+      />
     </div>
-  );
-};
+  </div>
+  </Link>
+
+   )    
+}
 
 export default Profile;
