1b6e95c70da2678a93875d5befc403af083fd5a8
sihayah <91216737+sihayah@users.noreply.github.com>
2022-05-31 23:35:06 -0400

    commented out Footer due to app breaking bugs therein

--- client/src/components/Navigator/index.js
+++ client/src/components/Navigator/index.js
@@ -1,57 +1,7 @@
 import React from "react";
-// import { Navbar, NavbarBrand, Nav, Container } from "react-bootstrap";
-// import { LinkContainer } from 'react-router-bootstrap'
 import "./navigator.css";
 import Auth from "../../utils/auth";
 import { NavLink } from "react-router-dom";
-
-// function Navigator() {
-//   const logout = event => {
-//     event.preventDefault();
-//     Auth.logout();
-//   };
-
-//   return (
-//     <div>
-//       <Navbar className="navBar">
-//         <Container className="navContainer">
-//           <Nav className="navLinks">
-//           <LinkContainer to="/" className="brand">
-//             <NavbarBrand>QRad</NavbarBrand>
-//             </LinkContainer>
-//             <LinkContainer to="/profile" className="navLink">
-//               <Nav.Link>my QRad</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to='/profile' className="navLink">
-//               <Nav.Link>profile</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to="/contacts" className="navLink">
-//               <Nav.Link>my contacts</Nav.Link>
-//             </LinkContainer>
-
-//             {Auth.loggedIn() ? (
-//             <>
-//             <a href="/" onClick={logout}>
-//               Log out
-//             </a>
-//             </>
-//           ) : (
-//             <>
-//             <LinkContainer>
-//               <Nav.Link to="/login" className="navLink">log in</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer>
-//               <Nav.Link to="/signup" className="navLink">sign up</Nav.Link>
-//             </LinkContainer>
-//             </>
-//           )}
-//           </Nav>
-//         </Container>
-//       </Navbar>
-//     </div>
-//   );
-// }
-// export default Navigator;
 
 function Navigator() {
   const logout = (event) => {
