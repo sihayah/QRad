7b28afd291f04bbe25ada0f9a08697779914b5ad
sihayah <91216737+sihayah@users.noreply.github.com>
2022-05-31 23:36:48 -0400

    removed buggy media queries, updated :root

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigator from "../Navigator";
-// import Contacts from "../Contacts";
 import Home from "../../components/Home";
 import LoginForm from "../Login";
 import Signup from "../Signup";
